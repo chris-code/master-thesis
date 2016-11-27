0f25e84356e8326236200a0741dc4570d2b70db0
Christian Mielers <christian.mielers@rub.de>
2016-11-27 20:30:12 +0100

    More thesis writing helper code

---
+++ report/compile_pdf.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+filename="Report"
+
+pdflatex "$filename".tex
+pdflatex "$filename".tex
+bibtex "$filename".aux
+pdflatex "$filename".tex
+pdflatex "$filename".tex
+
+# clean up
+rm "$filename".aux "$filename".bbl "$filename".blg "$filename".log "$filename".toc
