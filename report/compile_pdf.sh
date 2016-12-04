bf8cfe44a593a530a483ea917840c89b30f73959
Christian Mielers <christian.mielers@rub.de>
2016-12-04 22:13:15 +0100

    Fixed dangling paragraphcs etc.

--- report/compile_pdf.sh
+++ report/compile_pdf.sh
@@ -7,6 +7,7 @@ pdflatex "$filename".tex
 bibtex "$filename".aux
 pdflatex "$filename".tex
 pdflatex "$filename".tex
+pdflatex "$filename".tex
 
 # clean up
 rm "$filename".aux "$filename".bbl "$filename".blg "$filename".log "$filename".toc "$filename".lof "$filename".lot "$filename".loa
